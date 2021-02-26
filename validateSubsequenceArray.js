9998acdf7ff10d08bdae5d43cd6e17e082eaad6d
Linh Vu <thuylinh2490@gmail.com>
2021-02-26 00:02:39 -0500

    feat(promt): add validateSubsequenceArray

---
+++ validateSubsequenceArray.js
@@ -0,0 +1,14 @@
+function isValidSubsequence(array, sequence) {
+
+  let arrayIndex = 0;
+  let sequenceIndex =0;
+   while(arrayIndex< array.length && sequenceIndex < sequence.length){
+     if(array[arrayIndex] === sequence[sequenceIndex]){
+         sequenceIndex +=1;
+     }
+     arrayIndex+=1;
+   }
+     return sequenceIndex === sequence.length
+ }
+
+ isValidSubsequence()
