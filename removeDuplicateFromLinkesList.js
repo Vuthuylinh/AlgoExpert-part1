a307db176ad62392a0649c368b134fea810753d9
Linh Vu <thuylinh2490@gmail.com>
2021-03-11 21:49:29 -0500

    linkedList: remove duplicates from Linked List

---
+++ removeDuplicateFromLinkesList.js
@@ -0,0 +1,35 @@
+/*
+Remove Duplicates Frome Linked List
+
+You are given the head of a singly Linked lIst whose nodes are in sorted order with respect to their values. Write a function that returns a modified version of the Linked List that doesn't contain any nodes with duplicate values. The Linked List should be modified in place (you shouldn't create a brand new list) and the modified Linked List should still have its nodes sorted with respect to their values.
+Each LinkedList node has an integer value as well as a next node pointing to the next node in the list or to None/Null if it's the tail of the list
+
+
+input: linkedList sorted
+output: the input linkedList that already remove the duplicate value node.
+
+Example:
+input: 1->1->2->3->3->4
+output: 1->2->3->4
+*/
+
+class LinkedListNode{
+  constructor(val){
+    this.value =val;
+    this.next =null
+  }
+}
+// Solution 1: for input= sorted linkedlist
+function removeDuplicatesFromLinkedList(headLinkedList){
+ let currentNode = headLinkedList;
+ while(currentNode){
+   let nextDistinctNode = currentNode.next;
+   while(nextDistinctNode !==null && nextDistinctNode.value === currentNode.value){
+     nextDistinctNode = nextDistinctNode.next
+   }
+   currentNode.next = nextDistinctNode
+   currentNode = nextDistinctNode
+ }
+
+  return headLinkedList
+}
