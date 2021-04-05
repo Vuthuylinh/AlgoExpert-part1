4e0343edb3ea1fa499499801a75d7042718504ab
Linh Vu <thuylinh2490@gmail.com>
2021-04-04 22:30:47 -0400

    medium - array pf products

---
+++ Medium/ArrayOfProducts.js
@@ -0,0 +1,6 @@
+/*
+Write a fucntion that takes in a non-empty array of integers and returns an array of the same length, where each element in the output array is equal to the product of every other number in the input array.
+In other words, the value at output[i] is equal to product of every number in the input aray other than input[i]
+Note that you are expected to solve this problem without using devison.
+
+*/
